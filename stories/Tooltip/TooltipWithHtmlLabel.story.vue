1ffcb5d1e7264297c788dc5f044a74102d96a84f
RytisTarasevicius <139129643+RytisTarasevicius@users.noreply.github.com>
2024-06-21 16:38:24 +0300

    Create TooltipWithHtmlLabel.story.vue

---
+++ stories/Tooltip/TooltipWithHtmlLabel.story.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="buttons-group">
+        <Tooltip
+            v-for="placement in placements"
+            :key="placement"
+            delay=500
+            :placement="placement">
+            <template #label>
+                <b>I support html elements </b> &#128540;
+            </template>
+            <Button>Hover over me!</Button>
+        </Tooltip>
+    </div>
+</template>
+
+<script>
+    import Tooltip from '@/components/Tooltip/Tooltip';
+    import Button from '@/components/Button/Button';
+
+    export default {
+        name: 'TooltipWithHtmlLabelStory',
+        components: { Tooltip, Button },
+        data() {
+            return {
+                placements: ['top', 'right', 'bottom', 'left']
+            };
+        }
+    };
+</script>
+<style scoped>
+.buttons-group {
+  max-width: 800px;
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
